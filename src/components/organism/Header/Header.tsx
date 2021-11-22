b96c0580ee908c818ca51a9e896b07e55843cc10
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2021-11-22 19:58:16 +0300

    Add Content Spacer to Header

--- src/components/organism/Header/Header.tsx
+++ src/components/organism/Header/Header.tsx
@@ -33,44 +33,49 @@ export const Header: React.FC<HeaderProps> = (props) => {
   };
 
   return (
-    <header className="fixed top-0 w-full px-2 bg-background-100 bg-opacity-80 dark:bg-background-900 dark:bg-opacity-80 backdrop-filter backdrop-blur-lg">
-      <div className="flex items-center justify-between max-w-lg m-auto">
-        <div>
-          <label htmlFor="lang" className="inline-block uppercase-first">
-            {t("language")}:
-          </label>
-          <select
-            name="lang"
-            id="lang"
-            className="p-1 m-2 uppercase border-2 rounded-lg cursor-pointer focus:ring-2"
-            onChange={handleSelectLang}
-            defaultValue={router.locale}
+    <>
+      <header className="fixed top-0 w-full px-2 border-b-2 border-opacity-20 h-14 bg-background-100 bg-opacity-80 dark:bg-background-900 dark:bg-opacity-80 backdrop-filter backdrop-blur-lg">
+        <div className="flex items-center justify-between max-w-lg m-auto">
+          <div>
+            <label
+              htmlFor="lang"
+              className="hidden sm:inline-block uppercase-first"
+            >
+              {t("language")}:
+            </label>
+            <select
+              name="lang"
+              id="lang"
+              className="p-1 m-2 uppercase border-2 rounded-lg cursor-pointer focus:ring-2"
+              onChange={handleSelectLang}
+              defaultValue={router.locale}
+            >
+              {router.locales?.map((l) => (
+                <option value={l} key={l}>
+                  {l}
+                </option>
+              ))}
+            </select>
+          </div>
+          <a
+            className="link"
+            href="https://github.com/BerkeKaragoz/next-ts-tailwindcss-i18n"
           >
-            {router.locales?.map((l) => (
-              <option value={l} key={l}>
-                {l}
-              </option>
-            ))}
-          </select>
+            GitHub
+          </a>
+          <Switch
+            onClick={switchTheme}
+            defaultChecked={theme === "light"}
+            disabled={!isMounted}
+          >
+            <span className="uppercase-first">
+              {t("light mode", { ns: COMMON_TNS })}:
+            </span>
+          </Switch>
         </div>
-        <a
-          className="link"
-          href="https://github.com/BerkeKaragoz/next-ts-tailwindcss-i18n"
-        >
-          GitHub
-        </a>
-        <Switch
-          onClick={switchTheme}
-          defaultChecked={theme === "light"}
-          disabled={!isMounted}
-        >
-          <span className="uppercase-first">
-            {t("light mode", { ns: COMMON_TNS })}:
-          </span>
-        </Switch>
-      </div>
-      <hr className="opacity-40" />
-    </header>
+      </header>
+      <div className="h-14" />
+    </>
   );
 };
 
